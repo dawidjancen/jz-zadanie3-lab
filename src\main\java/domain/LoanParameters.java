16ca1d2b5549b6d7a2c48a857a61faf7f6cbb803
dawidjancen <jancen.dawid@gmail.com>
2017-04-09 13:19:38 +0200

    Initial commit!

---
+++ src\main\java/domain/LoanParameters.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class LoanParameters {
+
+	private int installmentCount;
+	private int ammount;
+	
+	public int getInstallmentCount() {
+		return installmentCount;
+	}
+	public void setInstallmentCount(int installmentCount) {
+		this.installmentCount = installmentCount;
+	}
+
+	public int getAmmount() {
+		return ammount;
+	}
+	public void setAmmount(int ammount) {
+		this.ammount = ammount;
+	}
+
+}
